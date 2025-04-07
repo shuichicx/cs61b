d7a1708ea6562569bb0ddb07ae77b11a15284118
shuichicx <3448771703@qq.com>
2025-04-08 01:41:14 +0800

    'done'

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,57 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> a = new AListNoResizing<>();
+        BuggyAList<Integer> b = new BuggyAList<>();
+        a.addLast(4);
+        b.addLast(4);
+        a.addLast(5);
+        b.addLast(5);
+        a.addLast(6);
+        b.addLast(6);
+        assertEquals(a.size(), b.size());
+        assertEquals(a.removeLast(), b.removeLast());
+        assertEquals(a.removeLast(), b.removeLast());
+        assertEquals(a.removeLast(), b.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 500;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = correct.size();
+                assertEquals(correct.size(), broken.size());
+                System.out.println("size: " + size);
+            }
+
+            if (correct.size() == 0)
+                continue;
+
+            if (operationNumber == 2) {
+                // getLast
+                int val = correct.getLast();
+                assertEquals(correct.getLast(), broken.getLast());
+                System.out.println("getLast: " + val);
+            } else if (operationNumber == 3) {
+                // removeLast
+                int val = correct.removeLast();
+                assertEquals(val, (int) broken.removeLast());
+                System.out.println("removeLast: " + val);
+            }
+        }
+    }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,24 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 1000; i <= 128000; i *= 2) {
+            Ns.addLast(i);
+            opCounts.addLast(10000);
+            SLList<Integer> a = new SLList<>();
+            for (int j = 0; j < i; j += 1) {
+                a.addLast(j);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int k = 0; k < 10000; k += 1) {
+                a.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,opCounts);
     }
 
 }
